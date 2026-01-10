1d8cf21fe03540b811c8ebb85c3540e394519ec4
BinzDBlvt08 <nhi.dakhalvt08@gmail.com>
2026-01-11 03:18:27 +0700

    Delete netcafe-backend directory
    Lỗi bên phía server

--- netcafe-backend/src/main/java/com/netcafe/backend/model/ChatMessage.java
+++
@@ -1,9 +0,0 @@
-package com.netcafe.backend.model;
-
-public class ChatMessage {
-    public String id;
-    public String fromUser;
-    public String toUser; // optional
-    public String message;
-    public long createdAt;
-}
